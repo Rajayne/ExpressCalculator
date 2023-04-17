3aee7b8421464decea86afed03cd88e6b07e3239
Rajayne <ryann.j.nealey@gmail.com>
2023-04-17 14:38:02 -0700

    Create 403, 404 and 500 error with try catch on routes

--- app.js
+++ app.js
@@ -1,36 +1,73 @@
 const express = require('express');
-const math = require('./math')
+const math = require('./math');
+const ExpressError = require('./error');
 const app = express();
 
 app.use(express.json());
 
 app.get('/mean', (req, res, next) => {
-    list = req.query.nums.split(",").map(Number)
-    mean = math.findMean(list);
-    return res.json({
-        response: {
-            operation: "mean", 
-            value: `${mean}`}
-    });
+    try {
+        list = req.query.nums.split(",").map(Number)
+        mean = math.findMean(list);
+        if (!mean) {
+            throw new ExpressError("Invalid Integer", 403)
+        }
+        return res.json({
+            response: {
+                operation: "mean", 
+                value: `${mean}`}
+        });
+    } catch (e) {
+        next(e);
+    }
 })
 
 app.get('/median', (req, res, next) => {
-    list = req.query.nums.split(",").map(Number)
-    median = math.findMedian(list);
-    return res.json({
-        response: {
-            operation: "median", 
-            value: `${median}`}
-    });
+    try {
+        list = req.query.nums.split(",").map(Number)
+        median = math.findMedian(list);
+        if (!median) {
+            throw new ExpressError("Invalid Integer", 403)
+        }
+        return res.json({
+            response: {
+                operation: "median", 
+                value: `${median}`}
+        });
+    } catch (e) {
+        next(e);
+    }
 })
 
 app.get('/mode', (req, res, next) => {
-    list = req.query.nums.split(",").map(Number)
-    mode = math.findMode(list);
-    return res.json({
-        response: {
-            operation: "mode", 
-            value: `${mode}`}
+    try {
+        list = req.query.nums.split(",").map(Number)
+        mode = math.findMode(list);
+        if (!mode) {
+            throw new ExpressError("Invalid Integer", 403)
+        }
+        return res.json({
+            response: {
+                operation: "mode", 
+                value: `${mode}`}
+        });
+    } catch (e) {
+        next(e);
+    }
+})
+
+app.use((req, res, next) => {
+    const e = new ExpressError("Page Not Found", 404)
+    next(e);
+})
+
+app.use((err, req, res, next) => {
+    // default status is 500 Internal Server Error
+    let status = err.status || 500;
+    let message = err.message;
+
+    return res.status(status).json({
+        error: {message, status}
     });
 })
 
