1e71997497c5bb537b0c12a8965d157551a29ec2
Rajayne <ryann.j.nealey@gmail.com>
2023-04-17 13:24:54 -0700

    Create mean route and find mean function

--- app.js
+++ app.js
@@ -2,3 +2,28 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+
+app.get('/mean', (req, res, next) => {
+    list = req.query.nums.split(",").map(Number)
+    mean = findMean(list);
+    return res.json({
+        response: {
+            operation: "mean", 
+            value: `${mean}`}
+    });
+})
+
+function findMean(list) {
+    let sum = 0;
+    let count = 0;
+    for (let i = 0; i < list.length; i++) {
+        sum += list[i];
+        count++;
+    }
+    mean = sum/count;
+    return mean;
+}
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+})
